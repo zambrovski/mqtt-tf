a38857e4d685d8a9956757ee0389127ff04d07c5
Simon Zambrovski <simon.zambrovski@holisticon.de>
2015-12-21 22:19:49 +0100

    expression changed

--- src/main/java/de/techjava/mqtt/tf/StarterConfiguration.java
+++ src/main/java/de/techjava/mqtt/tf/StarterConfiguration.java
@@ -4,8 +4,6 @@ import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
--- src/main/java/de/techjava/mqtt/tf/device/Voltmeter.java
+++ src/main/java/de/techjava/mqtt/tf/device/Voltmeter.java
@@ -22,9 +22,9 @@ import de.techjava.mqtt.tf.core.DeviceFactoryRegistry;
 public class Voltmeter implements DeviceFactory {
 
 	private Logger logger = LoggerFactory.getLogger(Voltmeter.class);
-	@Value("${tinkerforge.voltmeter.callbackperiod?: 100}")
+	@Value("#{tinkerforge.voltmeter.callbackperiod?:100}")
 	private long callbackperiod;
-	@Value("${tinkerforge.voltmeter.topic?:pressure}")
+	@Value("#{tinkerforge.voltmeter.topic?:'pressure'}")
 	private String topic;
 
 	@Autowired
@@ -49,6 +49,7 @@ public class Voltmeter implements DeviceFactory {
 		});
 		try {
 			sensor.setVoltageCallbackPeriod(realm.getCallback(uid, callbackperiod));
+
 		} catch (TimeoutException | NotConnectedException e) {
 			logger.error("Error setting callback period", e);
 		}
